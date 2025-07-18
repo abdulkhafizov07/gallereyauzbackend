8f2b5eb2b9976118239cdf2e9df3d3c8a33c29a2
M-chan <new.jasjan.programmer@gmail.com>
2025-07-18 11:15:22 +0500

    ...

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from sqlmodel import Session, SQLModel
+from sqlalchemy import create_engine
+from alembic.config import Config
+from alembic import command
+
+TEST_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///test_db.sqlite3")
+
+@pytest.fixture(scope="session")
+def engine():
+    engine = create_engine(TEST_DATABASE_URL, echo=False)
+    
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", TEST_DATABASE_URL)
+    command.upgrade(alembic_cfg, "head")
+    
+    yield engine
+
+@pytest.fixture(scope="function")
+def session(engine):
+    connection = engine.connect()
+    trans = connection.begin()
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    trans.rollback()
+    connection.close()
+
