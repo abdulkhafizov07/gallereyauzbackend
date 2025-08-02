89fa65178038db8c0608ec87aa8da7254cd29d1f
M-chan <new.jasjan.programmer@gmail.com>
2025-08-02 20:15:10 +0500

    removed unused libs from main requirements

--- models/image.py
+++ models/image.py
@@ -4,6 +4,7 @@ from .time_stamped_uuid import TimeStampedUUIDModel
 class ImageModel(TimeStampedUUIDModel, table=True):
     __tablename__ = "image_model"
 
+    image: str
     title: str
     short_description: str
     description: str
